3b558d6f929c9f98edca92054e9557edc87b93eb
Aditya7898 <aditya.cs.ind@gmail.com>
2019-03-04 18:21:15 +0530

    multiple images also included

--- app.js
+++ app.js
@@ -7,36 +7,44 @@ const app = express();
 
 // set storage engine
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/');
+    },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, 'Image' + Date.now() + path.extname(file.originalname));
     }
-})
-
-// Init upload
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1000000 },
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    }
-}).single('imageFile');
+});
 
 
-function checkFileType(file, cb) {
-    // Allowed extentions
-    const filetypes = /jpeg|jpg|png/;
-    // check ext.
-    const extname = filetypes.test(path.extname(file.originalname).toLocaleLowerCase());
-    // check MIME type
-    const mimetype = filetypes.test(file.mimetype);
+// multiple Images
+var upload = multer({
+    storage: storage
+});
 
-    if (mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: image only');
-    }
-}
+// Init upload for single image
+// const upload = multer({
+//     storage: storage,
+//     limits: { fileSize: 1000000 },
+//     fileFilter: function (req, file, cb) {
+//         checkFileType(file, cb);
+//     }
+// }).single('imageFile');
+
+
+// function checkFileType(file, cb) {
+//     // Allowed extentions
+//     const filetypes = /jpeg|jpg|png/;
+//     // check ext.
+//     const extname = filetypes.test(path.extname(file.originalname).toLocaleLowerCase());
+//     // check MIME type
+//     const mimetype = filetypes.test(file.mimetype);
+
+//     if (mimetype && extname) {
+//         return cb(null, true);
+//     } else {
+//         cb('Error: image only');
+//     }
+// }
 
 // ejs
 app.set('view engine', 'ejs');
@@ -44,28 +52,49 @@ app.set('view engine', 'ejs');
 // Public folder
 app.use(express.static('./public'));
 app.get('/', (req, res) => res.render('index'));
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            res.render('index', {
-                msg: err
-            });
-        } else {
-            console.log(req.file);
-            if (req.file == undefined) {
-                res.render('index', {
-                    msg: 'Error: no file selected'
-                });
-            } else {
-                res.render('index', {
-                    msg: 'File Uploaded',
-                    file: `uploads/${req.file.filename}`
-                });
-            }
-        }
-    });
-})
+
+// for multiple images
+app.post('/upload', upload.any(), function (req, res, next) {
+    console.log(req.files);
+    res.send(req.files);
+});
+
+// For single image
+// app.post('/upload', (req, res) => {
+//     upload(req, res, (err) => {
+//         if (err) {
+//             res.render('index', {
+//                 msg: err
+//             });
+//         } else {
+
+//             console.log(req.file);
+
+
+//             if (req.file == undefined) {
+//                 res.render('index', {
+//                     msg: 'Error: no file selected'
+//                 });
+//             } else {
+//                 res.render('index', {
+//                     msg: 'File Uploaded',
+//                     file: `uploads/${req.file.filename}`
+//                 });
+//             }
+//         }
+//     });
+// })
+
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
+
+
+// multiImage upload
+// var upload = multer({ dest: './public/uploads/' });
+
+// app.post('/upload', upload.any(), function (req, res, next) {
+//     console.log(req.files);
+//     res.send(req.files);
+// });
